64adcf2c07b57d6342d5841e5eccd8a2ac56e988
RoiLe <roilevi12@gmail.com>
2025-05-25 16:32:01 +0300

    Updates

---
+++ precache-manifest.b66ac8e5e580847d349ae7ea46d32f87.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae45e870d3abb5c2c3d168dae04d953",
+    "url": "/easycase-app/index.html"
+  },
+  {
+    "revision": "af360afab896c5525c91",
+    "url": "/easycase-app/static/css/main.eb4a71cc.chunk.css"
+  },
+  {
+    "revision": "18199ba6401f93006a16",
+    "url": "/easycase-app/static/js/2.3f5ab3fc.chunk.js"
+  },
+  {
+    "revision": "af360afab896c5525c91",
+    "url": "/easycase-app/static/js/main.4d8908e4.chunk.js"
+  },
+  {
+    "revision": "1eecb13891b3e83fd2fa",
+    "url": "/easycase-app/static/js/runtime~main.41586f07.js"
+  },
+  {
+    "revision": "19175cd18d01976c20364a79aee5d283",
+    "url": "/easycase-app/static/media/banner1.19175cd1.png"
+  },
+  {
+    "revision": "7c11d8ea371632ca433456565f782032",
+    "url": "/easycase-app/static/media/banner2.7c11d8ea.png"
+  },
+  {
+    "revision": "7c11d8ea371632ca433456565f782032",
+    "url": "/easycase-app/static/media/banner3.7c11d8ea.png"
+  },
+  {
+    "revision": "7c11d8ea371632ca433456565f782032",
+    "url": "/easycase-app/static/media/picture.7c11d8ea.png"
+  }
+]);
